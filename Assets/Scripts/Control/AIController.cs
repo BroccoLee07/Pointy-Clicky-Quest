3bfcb92010460998c08ecff114febfc6da0139b5
Marla Seras <BroccoLee07@users.noreply.github.com>
2024-02-20 22:57:55 +0800

    FEAT: Enemies guard a location and return to said location after player is out of range
    Some timer before enemy returns to guard location to imitate some sort of suspicion towards the player

--- Assets/Scripts/Control/AIController.cs
+++ Assets/Scripts/Control/AIController.cs
@@ -6,33 +6,61 @@ using UnityEngine;
 namespace RPG.Control {
     [RequireComponent(typeof(CharacterMovement))]
     [RequireComponent(typeof(CharacterCombat))]
-    [RequireComponent(typeof(Health))]
+    [RequireComponent(typeof(ActionScheduler))]
+    [RequireComponent(typeof(Health))]    
     public class AIController : MonoBehaviour {
         [SerializeField] private float chaseDistance = 5f;
+        [SerializeField] private float suspicionTime = 5f;
 
         // Dependency
         private CharacterCombat characterCombat;
         private CharacterMovement characterMovement;
+        private ActionScheduler actionScheduler;
         private Health health;
-
         private GameObject player;
+
+        private Vector3 guardPosition;
+        private float timeSinceLastDetectedPlayer = Mathf.Infinity;
+        
 
         void Start() {
             player = GameObject.FindWithTag("Player");
             characterCombat = GetComponent<CharacterCombat>();
             characterMovement = GetComponent<CharacterMovement>();
+            actionScheduler = GetComponent<ActionScheduler>();
             health = GetComponent<Health>();
+
+            guardPosition = transform.position;
         }
 
         void Update() {
             if (player == null) return;
             if (health.IsDead) return;
             
-            if (IsPlayerInDetectRange() && characterCombat.CanAttack(player)) {                
-                characterCombat.Attack(player);
+            if (IsPlayerInDetectRange() && characterCombat.CanAttack(player)) {
+                timeSinceLastDetectedPlayer = 0;
+                AttackBehaviour();
+            } else if (timeSinceLastDetectedPlayer <= suspicionTime) {
+                // Linger on doing nothing as if thinking or suspicious of player's action
+                SuspicionBehaviour();
             } else {
-                characterCombat.Cancel();
+                // Return to guard position if player is out of range or cannot be attacked
+                GuardBehaviour();
             }
+
+            timeSinceLastDetectedPlayer += Time.deltaTime;
+        }
+
+        private void GuardBehaviour() {
+            characterMovement.StartMoveAction(guardPosition);
+        }
+
+        private void SuspicionBehaviour() {
+            actionScheduler.CancelCurrentAction();
+        }
+
+        private void AttackBehaviour() {
+            characterCombat.Attack(player);
         }
 
         private bool IsPlayerInDetectRange() {
