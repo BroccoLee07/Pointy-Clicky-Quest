333c1e7e4bbd99b862e31c460ca5a36aec9e6069
Marla Seras <BroccoLee07@users.noreply.github.com>
2024-03-07 07:41:03 +0800

    FEAT: Portal from level 1 to level 2 scene

---
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace RPG.SceneManagement {
+    public class Portal : MonoBehaviour {
+
+        [SerializeField] private int sceneToLoad = -1;
+        private void OnTriggerEnter(Collider other) {
+            if (other.gameObject.tag == "Player") {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }
+    }
+}
+
+
