45a31f6512ad477a7c540efa210da969d3e4f72d
Marla Seras <BroccoLee07@users.noreply.github.com>
2024-03-07 08:07:55 +0800

    FEAT: Transition between scenes through invisible portal

--- Assets/Scripts/SceneManagement/Portal.cs
+++ Assets/Scripts/SceneManagement/Portal.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -5,10 +6,20 @@ namespace RPG.SceneManagement {
     public class Portal : MonoBehaviour {
 
         [SerializeField] private int sceneToLoad = -1;
+
+
         private void OnTriggerEnter(Collider other) {
             if (other.gameObject.tag == "Player") {
-                SceneManager.LoadScene(sceneToLoad);
+                StartCoroutine(Transition());
             }
+        }
+
+        private IEnumerator Transition() {
+            // Keep portal in the next scene until it is finished loading completely
+            DontDestroyOnLoad(gameObject);
+            yield return SceneManager.LoadSceneAsync(sceneToLoad);
+            Debug.Log("Scene Loaded");
+            Destroy(gameObject);
         }
     }
 }
