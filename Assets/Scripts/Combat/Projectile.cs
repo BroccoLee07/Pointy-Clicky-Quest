6b082f5a14c71f2d5703065b0636b6add8e2e69f
Marla Seras <BroccoLee07@users.noreply.github.com>
2024-03-20 20:52:34 +0800

    FEAT: Create homing and non-homing projectiles; Projectile continues on path when target is dead

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -6,22 +6,28 @@ using UnityEngine;
 namespace RPG.Combat {
     public class Projectile : MonoBehaviour {
         [SerializeField] private float speed = 5f;
-        // [SerializeField] private float damage = 1f;
+        [SerializeField] private bool isHoming = false;
         private Health target;
-        private float weaponDamage = 0;
+        private float weaponDamage = 0;        
 
         public void Initialize(Health targetHealth, float weaponDamage) {
             target = targetHealth;
             this.weaponDamage = weaponDamage;
+
+            if (!isHoming) {
+                // Set target once here for non-homing behaviour
+                transform.LookAt(GetAimLocation(target));
+            }            
         }
 
         void Update() {
             if (target == null) return;
 
-            Vector3 targetPos = GetAimLocation(target);
-            // Debug.Log($"targetPos: {targetPos}");
+            // Set projectile's target to update on every frame and have homing behaviour
+            if (isHoming && !target.IsDead) {
+                transform.LookAt(GetAimLocation(target));
+            }
 
-            transform.LookAt(targetPos);
             transform.Translate(Vector3.forward * speed * Time.deltaTime);
         }
 
@@ -37,6 +43,11 @@ namespace RPG.Combat {
 
         private void OnTriggerEnter(Collider other) {
             if (other.GetComponent<Health>() != target) return;
+
+            if (target.IsDead) {
+                // TODO: Destroy gameobject after a certain amount of time
+                return;
+            }
 
             // Damage will only be based on weapon's damage for now
             // In this case, the projectile contains the weapon data including damage
