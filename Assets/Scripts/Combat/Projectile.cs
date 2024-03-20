d652c5543025ead849dc65e8d985838168b123a8
Marla Seras <BroccoLee07@users.noreply.github.com>
2024-03-20 15:56:34 +0800

    FEAT: Trail for arrow projectile; Prefab variants for projectile; Create enemy archer prefab
    CHORE: Moved prefabs to appropriate folders

--- Assets/Scripts/Combat/Projectile.cs
+++ Assets/Scripts/Combat/Projectile.cs
@@ -6,7 +6,7 @@ using UnityEngine;
 namespace RPG.Combat {
     public class Projectile : MonoBehaviour {
         [SerializeField] private float speed = 5f;
-        [SerializeField] private float damage = 1f;
+        // [SerializeField] private float damage = 1f;
         private Health target;
         private float weaponDamage = 0;
 
@@ -38,7 +38,10 @@ namespace RPG.Combat {
         private void OnTriggerEnter(Collider other) {
             if (other.GetComponent<Health>() != target) return;
 
-            target.TakeDamage(weaponDamage + damage);
+            // Damage will only be based on weapon's damage for now
+            // In this case, the projectile contains the weapon data including damage
+            // target.TakeDamage(weaponDamage + damage);
+            target.TakeDamage(weaponDamage);
             Destroy(gameObject);
         }
     }
