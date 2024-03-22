00234d965bf81c3890bfa518d2fde08bf2c04a23
Marla Seras <BroccoLee07@users.noreply.github.com>
2024-03-22 19:22:17 +0800

    FEAT: Respawn pickups after a few seconds; FIX: Set correct scriptableobject for sword

--- Assets/Scripts/Combat/WeaponPickup.cs
+++ Assets/Scripts/Combat/WeaponPickup.cs
@@ -6,11 +6,40 @@ namespace RPG.Combat {
     
     public class WeaponPickup : MonoBehaviour {
         [SerializeField] private Weapon weapon;
+        [Tooltip("Main child component of weapon pickup that contains the renderer but not the WeaponPickup script")]
+        [SerializeField] private GameObject pickupChild;
+        [SerializeField] private float respawnTime = 5f;        
+
+        private Collider pickupCollider;
+
+        void Start() {
+            pickupCollider = GetComponent<Collider>();
+        }
+
         private void OnTriggerEnter(Collider other) {
             if (other.tag == "Player") {
                 other.GetComponent<CharacterCombat>().EquipWeapon(weapon);
-                Destroy(gameObject);
+                // Destroy(gameObject);
+                StartCoroutine(HideForSeconds(respawnTime));
             }
+        }
+
+        private IEnumerator HideForSeconds(float seconds) {
+            ShowPickup(false);
+            yield return new WaitForSeconds(seconds);
+            ShowPickup(true);
+        }
+
+        private void ShowPickup(bool isVisible) {
+            if (pickupChild == null || pickupCollider == null) return;
+
+            pickupCollider.enabled = isVisible;
+            pickupChild.SetActive(isVisible);
+
+        // Other implementation is to loop through all children to disable and hide the pickup
+        //     foreach (Transform child in transform) {
+        //         child.gameObject.SetActive(isVisible);
+        //     }
         }
     }
 }
