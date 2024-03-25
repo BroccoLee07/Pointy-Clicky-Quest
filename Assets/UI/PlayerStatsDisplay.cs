a2acde66f406ae8243f1a71f53e08c080f7b3512
Marla Seras <BroccoLee07@users.noreply.github.com>
2024-03-26 01:40:45 +0800

    FEAT: Display player's current level

--- Assets/UI/PlayerStatsDisplay.cs
+++ Assets/UI/PlayerStatsDisplay.cs
@@ -1,18 +1,22 @@
 using System;
 using RPG.Attributes;
+using RPG.Stats;
 using TMPro;
 using UnityEngine;
 
 namespace RPG.UI {
     public class PlayerStatsDisplay : MonoBehaviour {
         [SerializeField] TextMeshProUGUI healthValue;
+        [SerializeField] TextMeshProUGUI levelValue;
         [SerializeField] TextMeshProUGUI expValue;
         private Health health;
         private Experience experience;
+        private BaseStats baseStats;
 
         void Awake() {
             health = GameObject.FindWithTag("Player").GetComponent<Health>();
             experience = GameObject.FindWithTag("Player").GetComponent<Experience>();
+            baseStats = GameObject.FindWithTag("Player").GetComponent<BaseStats>();
         }
 
         void Update() {
@@ -23,6 +27,10 @@ namespace RPG.UI {
 
             if (expValue.text != experience.ExperiencePoints.ToString()){
                 expValue.text = experience.ExperiencePoints.ToString();
+            }
+
+            if (Int32.Parse(levelValue.text) != baseStats.GetLevel()) {
+                levelValue.text = baseStats.GetLevel().ToString();
             }
         }
     }
