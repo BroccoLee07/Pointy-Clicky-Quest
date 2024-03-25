ca1575e821e2a3824d98b25a180d4b2c7d7b123d
Marla Seras <BroccoLee07@users.noreply.github.com>
2024-03-25 21:28:09 +0800

    FEAT: Display player's exp on HUD

--- Assets/Scripts/Attributes/Experience.cs
+++ Assets/Scripts/Attributes/Experience.cs
@@ -1,13 +1,24 @@
-using System.Collections;
-using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using RPG.Saving;
 using UnityEngine;
 
 namespace RPG.Attributes {
-    public class Experience : MonoBehaviour {
+    public class Experience : MonoBehaviour, IJsonSaveable {
         [SerializeField] float experiencePoints = 0;
+
+        // Property
+        public float ExperiencePoints { get => experiencePoints; }
 
         public void GainExperience(float experiencePoints) {
             this.experiencePoints += experiencePoints;
+        }
+
+        public JToken CaptureAsJToken() {
+            return JToken.FromObject(experiencePoints);
+        }
+
+        public void RestoreFromJToken(JToken state) {
+            experiencePoints = state.ToObject<float>();
         }
     }
 }
--- Assets/UI/PlayerStatsDisplay.cs
+++ Assets/UI/PlayerStatsDisplay.cs
@@ -4,18 +4,25 @@ using TMPro;
 using UnityEngine;
 
 namespace RPG.UI {
-    public class PlayerHealthDisplay : MonoBehaviour {
+    public class PlayerStatsDisplay : MonoBehaviour {
         [SerializeField] TextMeshProUGUI healthValue;
+        [SerializeField] TextMeshProUGUI expValue;
         private Health health;
+        private Experience experience;
 
         void Awake() {
             health = GameObject.FindWithTag("Player").GetComponent<Health>();
+            experience = GameObject.FindWithTag("Player").GetComponent<Experience>();
         }
 
         void Update() {
             if (healthValue.text != String.Format("{0:0.00}%", health.GetPercentage())) {
                 // Debug.Log($"health percent: {health.GetPercentage()}");
-                healthValue.text = String.Format("{0:0.00}%", health.GetPercentage());
+                healthValue.text = String.Format("{0:0.00}%", health.GetPercentage());                
+            }
+
+            if (expValue.text != experience.ExperiencePoints.ToString()){
+                expValue.text = experience.ExperiencePoints.ToString();
             }
         }
     }
