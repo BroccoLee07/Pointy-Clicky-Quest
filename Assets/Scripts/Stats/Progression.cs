44dc90f706bc5f9bca9f69abf8c7b9d51a281587
Marla Seras <BroccoLee07@users.noreply.github.com>
2024-03-25 22:11:39 +0800

    FEAT: Build a lookup table for progression rather than looping through each time

--- Assets/Scripts/Stats/Progression.cs
+++ Assets/Scripts/Stats/Progression.cs
@@ -1,24 +1,40 @@
 using System;
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
 
 namespace RPG.Stats {
     [CreateAssetMenu(fileName = "Progression", menuName = "Stats/New Progression", order = 0)]
     public class Progression : ScriptableObject {
         [SerializeField] ProgressionCharacterClass[] characterClasses;
 
+        private Dictionary<CharacterClass, Dictionary<Stat, float[]>> lookupTable;
+
         public float GetStat(Stat stat, CharacterClass characterClass, int level) {
-            foreach (ProgressionCharacterClass progCharClass in characterClasses) {
-                if (progCharClass.characterClass != characterClass) continue;
-                foreach (ProgressionStat progStat in progCharClass.stats) {
-                    if (progStat.stat != stat) continue;
-                    // Handle index out of bounds
-                    if (progStat.levels.Length < level) continue;
-                    
-                    return progStat.levels[level - 1];
-                }                
+            BuildLookup();
+
+            float[] levels = lookupTable[characterClass][stat];
+            // Handle index out of bounds and give default value
+            if (levels.Length < level) {
+                return 0;
             }
 
-            return 0;
+            return levels[level - 1];
+        }
+
+        private void BuildLookup() {
+            if (lookupTable != null) return;
+
+            lookupTable = new Dictionary<CharacterClass, Dictionary<Stat, float[]>>();
+
+            foreach (ProgressionCharacterClass progCharClass in characterClasses) {
+                Dictionary<Stat, float[]> statLookupTable = new Dictionary<Stat, float[]>();
+                foreach (ProgressionStat progressionStat in progCharClass.stats) {
+                    // Debug.Log($"Class: {progCharClass.characterClass} Stat: {progressionStat.stat}");
+                    statLookupTable[progressionStat.stat] = progressionStat.levels;
+                }
+                lookupTable[progCharClass.characterClass] = statLookupTable;
+            }
         }
 
         [Serializable]
