97f576055ad4b67e698da5e0702d1eedbf03b03f
Marla Seras <BroccoLee07@users.noreply.github.com>
2024-03-31 18:48:14 +0800

    FEAT: Set cursor type and cursor mappings for player that changes on hover
    CHORE: Import cursor type asset

--- Assets/Scripts/Control/PlayerController.cs
+++ Assets/Scripts/Control/PlayerController.cs
@@ -4,12 +4,29 @@ using RPG.Combat;
 using RPG.Stats;
 using UnityEngine.AI;
 using RPG.Attributes;
+using System;
 
 namespace RPG.Control {
     [RequireComponent(typeof(CharacterMovement))]
     [RequireComponent(typeof(CharacterCombat))]
     [RequireComponent(typeof(Health))]
     public class PlayerController : MonoBehaviour {
+        // Mouse cursor state
+        public enum CursorType {
+            None,
+            Movement,
+            Combat
+        }
+
+        [Serializable]
+        public struct CursorMapping {
+            public CursorType cursorType;
+            public Texture2D texture;
+            public Vector2 hotspot;
+        }
+
+        [SerializeField] private CursorMapping[] cursorMappings;
+        
         private CharacterMovement characterMovement;
         private CharacterCombat characterCombat;
         private Health health;
@@ -30,7 +47,7 @@ namespace RPG.Control {
             if (ProcessCombat()) return;
             if (ProcessMovement()) return;
 
-            // Debug.Log("Nothing to do.");
+            SetCursor(CursorType.None);
         }
 
         private bool ProcessCombat() {
@@ -47,8 +64,10 @@ namespace RPG.Control {
                     characterCombat.Attack(target.gameObject);                    
                 }
 
+                // Handles mouse hovers
+                SetCursor(CursorType.Combat);
+
                 // Return here since we are still handling/executing the ProcessCombat
-                // This includes hovering on a target and not clicking yet
                 return true;
             }
 
@@ -68,11 +87,28 @@ namespace RPG.Control {
                     characterMovement.StartMoveAction(raycastHit.point, 1f);
                 }
 
-                // Mouse could hover over terrain
+                // Handles mouse hovers
+                SetCursor(CursorType.Movement);
                 return true;
             }
 
             return false;
+        }
+
+        // Updates what cursor looks like based on what it hovers or the cursor type
+        private void SetCursor(CursorType cursorType) {
+            CursorMapping cursorMapping = GetCursorMapping(cursorType);
+            Cursor.SetCursor(cursorMapping.texture, cursorMapping.hotspot, CursorMode.Auto);
+        }
+
+        private CursorMapping GetCursorMapping(CursorType cursorType) {
+            foreach (CursorMapping mapping in cursorMappings) {
+                if (mapping.cursorType == cursorType) {
+                    return mapping;
+                }
+            }
+
+            return cursorMappings[0];
         }
     }
 }
