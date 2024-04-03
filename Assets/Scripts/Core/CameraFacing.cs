38a0cea107f0b72506dd1ae31d9a1fa42fca114f
Marla Seras <BroccoLee07@users.noreply.github.com>
2024-04-03 19:07:18 +0800

    FEAT: Damage text UI on top of characters in game facing towards the camera all times

---
+++ Assets/Scripts/Core/CameraFacing.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+namespace RPG.Core {
+    public class CameraFacing : MonoBehaviour {
+        void Update() {
+            transform.forward = Camera.main.transform.forward;
+        }
+    }
+}
