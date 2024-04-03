6504b163d71871d0f413f983b8079b62caf810cf
Marla Seras <BroccoLee07@users.noreply.github.com>
2024-04-03 22:30:19 +0800

    FEAT: Hide health bar on start and on death; Show when taking damage
    CHORE: Updated HUD to remove enemy health

--- Assets/Scripts/UI/HealthBar.cs
+++ Assets/Scripts/UI/HealthBar.cs
@@ -6,16 +6,39 @@ namespace RPG.UI {
 
         [SerializeField] private Health health;
         [SerializeField] private RectTransform foregroundTransform;
+        [SerializeField] private Canvas healthBarCanvas;
+
+        void Start() {
+            // Make sure health bar is hidden in the beginning
+            Show(false);
+        }
 
         void Update() {
             if (foregroundTransform.localScale.x == health.GetFraction()) return;
+            // If taking damage and health bar is not yet visible, show health bar
+            if (!IsVisible()) {
+                Show(true);
+            }
 
             UpdateHealthBar();
+
+            // If target is dead, hide health bar
+            if (health.IsDead) {
+                Show(false);
+            }
         }
 
         private void UpdateHealthBar() {
             // Set x value of local scale to update foreground of health bar
             foregroundTransform.localScale = new Vector3(health.GetFraction(), foregroundTransform.localScale.y, foregroundTransform.localScale.z);
+        }
+
+        private void Show(bool isVisible) {
+            healthBarCanvas.enabled = isVisible;
+        }
+
+        private bool IsVisible() {
+            return healthBarCanvas.enabled;
         }
     }
 }
