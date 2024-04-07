aac66170617c1c0277fd9bcf1e171478332dab5c
Marla Seras <BroccoLee07@users.noreply.github.com>
2024-04-07 14:31:02 +0800

    FIX: Save state of cinematic so it doesn't get triggered again after loading

--- Assets/Scripts/Cinematics/CinematicTrigger.cs
+++ Assets/Scripts/Cinematics/CinematicTrigger.cs
@@ -1,17 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
+using Newtonsoft.Json.Linq;
+using RPG.Saving;
 using UnityEngine;
 using UnityEngine.Playables;
 
 namespace RPG.Cinematics {
-    public class CinematicTrigger : MonoBehaviour {
-        private bool isAlreadyTriggered = false;
+    public class CinematicTrigger : MonoBehaviour, IJsonSaveable {
+        public bool isAlreadyTriggered = false;
+
 
         private void OnTriggerEnter(Collider other) {
             if(!isAlreadyTriggered && other.gameObject.tag == "Player") {
                 isAlreadyTriggered = true;
                 GetComponent<PlayableDirector>().Play();
             }            
+        }
+
+        public JToken CaptureAsJToken() {
+            return JToken.FromObject(isAlreadyTriggered);
+        }
+
+        public void RestoreFromJToken(JToken state) {
+            isAlreadyTriggered = state.ToObject<bool>();
         }
     }
 }
