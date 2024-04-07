4a7531cf3f2de789e288eff019a5a9f88dfa5ce9
Marla Seras <BroccoLee07@users.noreply.github.com>
2024-04-07 20:56:28 +0800

    FIX: Pause scene when main menu/controls are visible

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -4,28 +4,6 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class MainMenu : MonoBehaviour {
-    // [SerializeField] SavingWrapper saveControl;
-    // public void OnNewGameButtonClick() {
-    //     // saveControl.Delete();
-    //     // TODO: Restart game here
-    //     Debug.Log("New Game");
-    // }
-
-    // public void OnLoadGameButtonClick() {
-    //     saveControl.Load();
-    //     Debug.Log("Load Game");
-    // }
-
-    // public void OnInstructionsButtonClick() {
-    //     // TODO: Show instructions UI
-    //     Debug.Log("Instructions");
-    // }
-
-    // public void OnExitGameButtonClick() {
-    //     // TODO: Show exit game confirmation popup
-    //     Debug.Log("Exit Game");
-    // }
-
     [SerializeField] private GameObject MainMenuObject;
 
     private bool isVisible = false;
@@ -44,5 +22,11 @@ public class MainMenu : MonoBehaviour {
     private void ToggleMainMenu() {
         isVisible = !isVisible;
         MainMenuObject.SetActive(isVisible);
+
+        if (isVisible) {
+            Time.timeScale = 0f;
+        } else {
+            Time.timeScale = 1f;
+        }
     }
 }
